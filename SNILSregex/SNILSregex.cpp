d50aa67fc7410e0f0e0918670469ab00f447c5a1
F3llsy <dimaschostov2@gmail.com>
2023-11-10 17:51:30 +0300

    Add project files.

---
+++ SNILSregex/SNILSregex.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    string snils;
+    cout << "ENTER SNILS: ";
+    cin >> snils;
+
+    // Проверка на корректность с использованием регулярного выражения
+    regex regex("[0-9]{3}-[0-9]{3}-[0-9]{3} [0-9]{2}");
+    if (regex_match(snils, regex)) {
+        cout << "SNILS is correct!" << endl;
+    }
+    else {
+        cout << "SNILS is incorrect!" << endl;
+    }
+
+    return 0;
+}
